00d81583ad16b1190703e5c139cdebf95e34aae0
codeangularden <ganeshkraskar@rediffmail.com>
2016-04-25 21:28:54 +0530

    Added files via upload

---
+++ BSon.js
@@ -0,0 +1,15 @@
+﻿        public bool SaveDocumentBsonArray(string objectToSave, string collectionName)
+        {
+            var collection = mongoDB.GetCollection<BsonArray>(collectionName);
+            //collection.RemoveAll();
+            var netxMessageBatch = BsonSerializer.Deserialize<BsonArray>(objectToSave);
+            collection.InsertBatch(netxMessageBatch);   
+            return true;
+            //var collection1 = mongoDB.GetCollection<BsonDocument>(collectionName);
+            //MongoCursor<BsonDocument> cursor = collection1.FindAll();
+            //List<BsonDocument> entities = cursor.ToList();
+            //JavaScriptSerializer Json = new JavaScriptSerializer();
+            //string returnDocs = Json.Serialize(entities);
+
+        }
+
